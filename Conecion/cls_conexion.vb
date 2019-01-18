d72543a14c4d1708cc6cf3bb61e23bae570674d4
renato Cohaila <renatoclt@gmail.com>
2019-01-18 10:38:41 -0500

    mejora de reporte

--- Conecion/cls_conexion.vb
+++ Conecion/cls_conexion.vb
@@ -12,11 +12,11 @@ Public Class cls_conexion
     Public Sub New()
         Dim reg As New cls_registro
         Try
-            'Port = "3306"
-            'Server = "192.168.1.112"
-            'Database = "asdi" 'reg.leer("database")
-            'User = "root"
-            'Password = "YES"
+            Port = "3306"
+            Server = "192.168.1.112"
+            Database = "asdi" 'reg.leer("database")
+            User = "root"
+            Password = "YES"
 
             'Port = "3306"
             'Server = "192.168.1.112"
@@ -25,11 +25,11 @@ Public Class cls_conexion
             'Password = "YES"
 
 
-            Port = "3306"
-            Server = "localhost"
-            Database = "asdi" 'reg.Leer("Database")
-            User = "root"
-            Password = ""
+            'Port = "3306"
+            'Server = "localhost"
+            'Database = "asdi" 'reg.Leer("Database")
+            'User = "root"
+            'Password = ""
         Catch ex As Exception
             MsgBox("No se puede leer los parametros de coneccion")
         End Try
